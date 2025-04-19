b288ea96442292cf8ed41df30be5ad16c6880672
AnthonyA <alicea.9a@gmail.com>
2025-04-19 15:27:50 -0600

    fix: Add basic http authentication for admin index page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,3 +74,9 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 end
+
+
+# Load .env file in development and test environments
+if Rails.env.development? || Rails.env.test?
+  Dotenv::Railtie.load
+end
