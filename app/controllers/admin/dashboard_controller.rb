b840e557ceff9b1cf641ca333dd96043b316750e
AnthonyA <alicea.9a@gmail.com>
2025-04-20 14:45:07 -0600

    feat: Update admin page to display general site stats

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,12 @@
 class Admin::DashboardController < ApplicationController
   def show
+    product_and_category_count()
+  end
+
+  def product_and_category_count 
+    @product_count = Product.count
+    @category_count = Category.count
+    puts @product_count
+    puts @category_count
   end
 end
